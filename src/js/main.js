c3400591a5447c14187cccf23fb8aeff0276646e
Эдуард Жулега <bessistemn1i@gmail.com>
2016-11-30 22:24:11 +0200

    home commit

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,6 @@
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/jquery-rcrumbs/dist/jquery.rcrumbs.min.js
 //= ../../src/js/partials/jquery.maskedinput.min.js
-//= ../../bower_components/gmaps/gmaps.min.js
-//= ../../bower_components/superfish/dist/js/superfish.js
 /* Custom*/
 var $jq = jQuery.noConflict(true);
 
