bc559e443080a89d10005c6776b03c59a79e01a4
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2017-12-12 16:02:52 +0200

    second

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
 //= ../../bower_components/jquery/dist/jquery.js
-//= ../../bower_components/jquery-rcrumbs/dist/jquery.rcrumbs.min.js
-//= ../../src/js/partials/jquery.maskedinput.min.js
-
-//= ../../bower_components/jQuery-Feature-Carousel/js/jquery.featureCarousel.js
+//= ../../bower_components/slick-carousel/slick/slick.js
+//= ../../bower_components/jQuery-Waterwheel-Carousel-master/js/jquery.waterwheelCarousel.js
+//= ../../bower_components/jQuery-Waterwheel-Carousel-master/js/TweenMax.min.js
 /* Custom*/
 $(document).ready(function(){
-
 	// menu-icon
 
 	$('.nav-icon').click(function(){
 		$(this).toggleClass('open');
 		$('#menu').fadeToggle('opened-menu');
+	});
+	// carousel
+
+$("#carousel").waterwheelCarousel({
+
 	});
 
 	// slider 
@@ -20,35 +23,6 @@ $(document).ready(function(){
 		mobileFirst: true
 	});
 
-	// carousel
-
-$("#carousel").waterwheelCarousel({
-flankingItems: 3
-});
-$(window).bind("resize", resizeWindow);
-function resizeWindow(e) {
-var viewportWidth = $(window).width();
-if (viewportWidth <= 480) {
-carousel.reload({
-flankingItems: 0,
-})
-}
-else if (viewportWidth <= 767) {
-carousel.reload({
-flankingItems: 1,
-})
-}
-else if (viewportWidth <= 991) {
-carousel.reload({
-flankingItems: 2,
-})
-}
-else {
-carousel.reload({
-flankingItems: 3,
-})
-}
-}
 });
 
 
