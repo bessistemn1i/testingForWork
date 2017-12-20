23ffaa06305550751edc7f58b3c435d4002f3302
bessistemn1i <bessistemn1i@gmail.com>
2017-12-20 23:42:11 +0200

    svg

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
 //= ../../bower_components/jquery/dist/jquery.js
+//= ../../bower_components/progressbar.js/dist/progressbar.min.js
+//= ../../bower_components/jQuery-viewport-checker/dist/jquery.viewportchecker.min.js
 //= ../../bower_components/slick-carousel/slick/slick.js
 //= ../../bower_components/swiper/dist/js/swiper.js
 /* Custom*/
@@ -19,21 +21,51 @@ $(document).ready(function(){
 
 	// swiper
 
-var swiper = new Swiper('.swiper-container', {
-  pagination: '.swiper-pagination',
-  effect: 'coverflow',
-  grabCursor: true,
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  loop: true,
-  pagination: {
-  	el: '.swiper-pagination',
-  	type: 'bullets',
-  	clickable: true
-  }
-});
-});
+	var swiper = new Swiper('.swiper-container', {
+	  pagination: '.swiper-pagination',
+	  effect: 'coverflow',
+	  grabCursor: true,
+	  centeredSlides: true,
+	  slidesPerView: 'auto',
+	  loop: true,
+	  pagination: {
+	  	el: '.swiper-pagination',
+	  	type: 'bullets',
+	  	clickable: true
+	  }
+	});
 
+	// Progressbar 
+
+	$('.education-wrapper').viewportChecker({
+			callbackFunction: function(elem, action){
+		var read = new ProgressBar.Circle('#read', {
+			strokeWidth: 6,
+			color: '#fff',
+			duration: 1400,
+			trailWidth: 6,
+			trailColor: '#1b6a9e'
+		});
+		read.animate(.1);
+		var seen = new ProgressBar.Circle('#seen', {
+			strokeWidth: 6,
+			color: '#fff',
+			duration: 1400,
+			trailWidth: 6,
+			trailColor: '#1b6a9e'
+		});
+		seen.animate(.4);
+		var feel = new ProgressBar.Circle('#feel', {
+			strokeWidth: 6,
+			color: '#fff',
+			duration: 1400,
+			trailWidth: 6,
+			trailColor: '#1b6a9e'
+		});
+		feel.animate(.7)
+	}
+});
+});
 
 
 
