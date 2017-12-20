476d9b78caa19fa03df7f5dad072a425fef1b61a
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2017-12-20 16:36:48 +0200

    tools

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/slick-carousel/slick/slick.js
-//= ../../bower_components/jQuery-Waterwheel-Carousel-master/js/jquery.waterwheelCarousel.js
-//= ../../bower_components/jQuery-Waterwheel-Carousel-master/js/TweenMax.min.js
+//= ../../bower_components/swiper/dist/js/swiper.js
 /* Custom*/
 $(document).ready(function(){
 	// menu-icon
@@ -10,19 +9,29 @@ $(document).ready(function(){
 		$(this).toggleClass('open');
 		$('#menu').fadeToggle('opened-menu');
 	});
-	// carousel
 
-$("#carousel").waterwheelCarousel({
-
-	});
-
-	// slider 
+// slider 
 	$('.slider').slick({
 		arrows: false,
 		dots: true,
 		mobileFirst: true
 	});
 
+	// swiper
+
+var swiper = new Swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: true,
+  pagination: {
+  	el: '.swiper-pagination',
+  	type: 'bullets',
+  	clickable: true
+  }
+});
 });
 
 
