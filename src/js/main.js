a815388769d710d650837a2bd8367fbec510183e
unknown <C:\Users\user\AppData\Roaming\The Bat!>
2017-12-21 16:12:24 +0200

    expander

--- src/js/main.js
+++ src/js/main.js
@@ -44,7 +44,37 @@ $(document).ready(function(){
 			color: '#fff',
 			duration: 1400,
 			trailWidth: 6,
-			trailColor: '#1b6a9e'
+			trailColor: '#1b6a9e',
+			from: {color: '#fff', width: 6},
+			to: {color: '#fff', width: 6},
+			step: function(state, circle){
+				circle.path.setAttribute('stroke', state.color);
+				circle.path.setAttribute('stroke-width', state.width);
+
+				var value = Math.round(circle.value() * 100);
+				if(value === 0){
+					circle.setText('');
+				} else{
+					circle.setText(value);
+				}
+			},
+			text: {
+	            style: {
+	                color: null,
+	                position: 'absolute',
+	                left: '50%',
+	                top: '50%',
+	                padding: 0,
+	                transform: {
+	                    prefix: true,
+	                    value: 'translate(-50%, -50%)'
+	                }
+	            },
+	            autoStyleContainer: true,
+	            alignToBottom: true,
+	            value: null,
+	            className: 'progressbar-text'
+	        },
 		});
 		read.animate(.1);
 		var seen = new ProgressBar.Circle('#seen', {
@@ -52,19 +82,94 @@ $(document).ready(function(){
 			color: '#fff',
 			duration: 1400,
 			trailWidth: 6,
-			trailColor: '#1b6a9e'
+			trailColor: '#1b6a9e',
+			from: {color: '#fff', width: 6},
+			to: {color: '#fff', width: 6},
+			step: function(state, circle){
+				circle.path.setAttribute('stroke', state.color);
+				circle.path.setAttribute('stroke-width', state.width);
+
+				var value = Math.round(circle.value() * 100);
+				if(value === 0){
+					circle.setText('');
+				} else{
+					circle.setText(value);
+				}
+			},
+        	text: {
+            	style: {
+            	    color: null,
+            	    position: 'absolute',
+            	    left: '50%',
+            	    top: '50%',
+            	    padding: 0,
+            	    transform: {
+            	        prefix: true,
+            	        value: 'translate(-50%, -50%)'
+            	    }
+            	},
+            	autoStyleContainer: true,
+            	alignToBottom: true,
+            	value: null,
+            	className: 'progressbar-text'
+       		 },
 		});
-		seen.animate(.4);
+		seen.animate(.3);
 		var feel = new ProgressBar.Circle('#feel', {
 			strokeWidth: 6,
 			color: '#fff',
 			duration: 1400,
 			trailWidth: 6,
-			trailColor: '#1b6a9e'
+			trailColor: '#1b6a9e',
+			from: {color: '#fff', width: 6},
+			to: {color: '#fff', width: 6},
+			step: function(state, circle){
+				circle.path.setAttribute('stroke', state.color);
+				circle.path.setAttribute('stroke-width', state.width);
+
+				var value = Math.round(circle.value() * 100);
+				if(value === 0){
+					circle.setText('');
+				} else{
+					circle.setText(value);
+				}
+			},
+			text: {
+	            style: {
+	                color: null,
+	                position: 'absolute',
+	                left: '50%',
+	                top: '50%',
+	                padding: 0,
+	                transform: {
+	                    prefix: true,
+	                    value: 'translate(-50%, -50%)'
+	                }
+	            },
+	            autoStyleContainer: true,
+	            alignToBottom: true,
+	            value: null,
+	            className: 'progressbar-text'
+	        },
 		});
-		feel.animate(.7)
+		feel.animate(.9)
 	}
 });
+
+	// Portfolio
+	var descr = $('.descr');
+	$('.read-more').on('click', function() {
+		$('.read-more').slideDown();
+	});
+	var items = $('.item'),
+		per = 3,
+		i = 1,
+		total = 0;
+	$('.load-more').on('click', function(){
+	total = per * (i++);
+	items.slice(0, total).show();
+    $(this)[total >= items.length ? 'hide' : 'show']();
+}).click();
 });
 
 
